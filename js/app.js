1410242890f6319b44f3cb5662235be3be468359
mikky_lifestyle <mike2serg@gmail.com>
2018-08-05 23:10:33 +0100

    responsive @ 480px

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ $('.owl-carousel').owlCarousel({
     margin:10,
     nav:true,
     items:4,
-    // responsive:{
-    //     0:{
-    //         items:1
-    //     },
-    //     600:{
-    //         items:3
-    //     },
-    //     1000:{
-    //         items:10
-    //     }
-    // }
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:10
+        }
+    }
 })
 
 var owl = $('.owl-carousel');
